d730db2ebec49a0332651dccce3a5163f49a9b6d
Anjali Raikwar <167967116+ANJI1026@users.noreply.github.com>
2025-08-17 11:17:54 +0530

    Update page.tsx

--- doct/app/doctor/appointment/page.tsx
+++ doct/app/doctor/appointment/page.tsx
@@ -264,8 +264,4 @@ export default function DoctorDashboard() {
       </div>
     </main>
   );
-
-  function capitalizeFirst(str: string) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
 }
